b0a8fda0655e437fadfa2c86c7a1231ed1deb8fc
Mark J Menger <mark.j.menger@gmail.com>
2022-06-15 04:17:54 +0000

    streamline status of pre-check

--- tgw-workload/pre-check.sh
+++ tgw-workload/pre-check.sh
@@ -3,18 +3,16 @@
 # Check that site is online before creating workload resources
 #
 
-for x in `seq 1 120`; do
+for x in `seq 1 3`; do
     site_state=$(curl --location --request GET $VOLT_API_URL/config/namespaces/system/aws_tgw_sites/$TF_VAR_projectPrefix-tgw-1  -H "Authorization: APIToken $VOLTERRA_TOKEN" -H "content-type: application/json" -s  |jq -r .spec.site_state)
 if [ "$site_state" = "ONLINE" ]; then
    echo "ONLINE: AWS TGW site is online.  Safe to proceed. waited $x minutes"
    exit 0
 else
-    echo $site_state
-    echo "OFFLINE: wait for AWS TGW Site to be ONLINE before proceeding.  Waiting $x minutes"
+    echo "$site_state: wait for AWS TGW Site to be ONLINE before proceeding.  Waiting $x minutes"
     #exit 1
 fi    
 sleep 60;
 done;
-echo $site_state
-echo "OFFLINE: wait for AWS TGW Site to be ONLINE before proceeding; timed out after 120 minutes"
+echo "$site_state: wait for AWS TGW Site to be ONLINE before proceeding; timed out after 120 minutes"
 exit 1;
